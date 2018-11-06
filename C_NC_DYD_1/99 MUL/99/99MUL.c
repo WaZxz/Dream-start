1961ab1495d1bbb82f8e9b59cef4120e8e77e7b6
WaZxz <44762086+WaZxz@users.noreply.github.com>
2018-11-06 09:46:51 +0800

    Update and rename C_NC_day1/99MUL/99/99MUL.c to C_NC_DYD_1/99 MUL/99/99MUL.c

--- C_NC_DYD_1/99 MUL/99/99MUL.c
+++ C_NC_DYD_1/99 MUL/99/99MUL.c
@@ -3,8 +3,10 @@
 #include<stdlib.h>
 int main(){
 	int i = 0, j = 0;
-	for (i = 1; i <=9; i++){
-		for (j = 1; j <= i; j++){
+	for (i = 1; i <=9; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
 			printf("%d*%d=%d ",i,j,i*j);
 		}
 		printf("\n");
